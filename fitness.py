670663f5dba9ab8c2a644a0d39b8a63401dde045
SeongHo Jin <snskrktja@naver.com>
2022-03-31 14:20:48 +0900

    적합도 계산 방식 알맞게 수정

--- fitness.py
+++ fitness.py
@@ -1,14 +1,15 @@
+from statistics import mean
 from data_loader import red_point_list
 
 
 w_list = [0.2331, 0.2646, 0.1323, 0.2072, 0.1628]
-dummy_Cz_list = [50, 25, 50, 10, 55] # TODO
+Cz_list = [45, 130, 37, 11, 45]
 alpha = 0.3 # TODO
 
 
 def get_fitness(gene_data):
     total_fitness = 0
-    has_calculated_list = [ [ False for _ in range(5) ] for _ in range(3000) ]
+    is_calculated_list = [ [ False for _ in range(5) ] for _ in range(3000) ]
 
     for i in range(170):
         use_case = gene_data[i]
@@ -16,21 +17,27 @@ def get_fitness(gene_data):
         if use_case == -1:
             continue
 
+        total_D = 0
+        expected_time_list = []
         for point_info in red_point_list[i].possible_point_list:
             bluePoint = point_info['bluePoint']
             distance = point_info['distance']
             expected_time = point_info['expected_time']
 
             # 파란점이 서비스 범위 밖이면 넘어감
-            if distance > dummy_Cz_list[use_case]:
+            if distance > Cz_list[use_case]:
                 continue
 
             # 현재 파란점이 이미 "use_case"시설에 대해 계산되었다면 넘어감
-            if has_calculated_list[bluePoint.point_index - 1][use_case]:
+            if is_calculated_list[bluePoint.point_index - 1][use_case]:
                 continue
 
-            total_fitness += w_list[use_case] * (bluePoint.D_list[use_case] - expected_time * alpha)
+            total_D += bluePoint.D_list[use_case]
+            if expected_time != -1:
+                expected_time_list.append(expected_time)
 
-            has_calculated_list[bluePoint.point_index - 1][use_case] = True
+            is_calculated_list[bluePoint.point_index - 1][use_case] = True
 
-    return total_fitness / 100
+        total_fitness += w_list[use_case] * (total_D - mean(expected_time_list) * alpha)
+
+    return total_fitness
