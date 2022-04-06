b25801420ea5dea075db5ee2a6db6bae5bd42acc
SeongHo Jin <snskrktja@naver.com>
2022-04-06 22:40:44 +0900

    빨간점에 A_list를 추가하여 적합도 계산에 드는 시간을 줄임

--- fitness.py
+++ fitness.py
@@ -20,29 +20,21 @@ def get_fitness(gene_data, time_type):
             continue
 
         point_info_list = red_point_list[i].possible_point_list[:]
-        if use_case == 1: # 버스일 경우 far_point_list 추가
+
+        # 버스일 경우 far_point_list 추가
+        if use_case == 1:
             point_info_list += red_point_list[i].far_point_list[:]
             bus_weight = BUS_WEIGHT
 
         total_D = 0
-        car_expected_time_list = []
-        walk_expected_time_list = []
         for point_info in point_info_list:
             index = point_info['point_index'] - 1
-            isRoad = point_info['isRoad']
             D_list = point_info['D_list']
             distance = point_info['distance']
-            expected_time = point_info['expected_time']
 
             # 파란점이 서비스 범위 밖이면 넘어감
             if distance > Cz_list[use_case] + 1: # 오차범위 1m
                 continue
-
-            if expected_time != -1: # use_case가 1일 경우 소요시간 무시
-                if isRoad:
-                    car_expected_time_list.append(expected_time)
-                else:
-                    walk_expected_time_list.append(expected_time)
 
             # use_case에 대해 계산된 적 없는 파란점일 때만 total_D에 추가
             if not is_calculated_list[index][use_case]:
@@ -50,7 +42,7 @@ def get_fitness(gene_data, time_type):
 
                 is_calculated_list[index][use_case] = True
 
-        A = mean(car_expected_time_list) * mean(walk_expected_time_list) * ALPHA * bus_weight
+        A = red_point_list[i].A_list[use_case] * ALPHA * bus_weight
         total_fitness += w_list[use_case] * (total_D - A)
 
     return total_fitness
